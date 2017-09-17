11dba25db632e994e4e19ed3e7baf24d57d5249f
Ricardo <ricardo.de.almeida@tecnico.ulisboa.pt>
2017-09-17 16:06:18 +0100

    new

---
+++ labs/1/1.c
@@ -0,0 +1,25 @@
+Welcome, you are inside now.
+1st Task: Get the host name!
+You have 10 minutes.
+
+
+
+#include <unistd.h>
+int gethostname(char *name,size_t len);
+
+
+//test.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+extern int errno;
+int main(void)
+{
+char buffer[128];
+if(gethostname(buffer,128)==-1)
+printf(“error: %s\n",strerror(errno));
+else printf(“host name: %s\n",buffer);
+exit(0);
+}
