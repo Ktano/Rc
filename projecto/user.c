b0d2bc106216b1fb1e7413170a6ecf37bdce964e
Pedro <pedrocaetan56@gmail.com>
2017-09-24 12:59:30 +0100

    reads parameters from commandline

--- projecto/user.c
+++ projecto/user.c
@@ -1,1 +1,25 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+#define DEFAULT_PORT 58006
+#define BUFFER_MAX 80
+
+int main(int argc, char **argv){
+    int port= DEFAULT_PORT,i;
+    char *servername;
+
+    for (i = 0; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+        if(strcmp(argv[i],"-p")==0) port=atoi(argv[i+1]);
+        if(strcmp(argv[i],"-n")==0) servername=argv[i+1];
+    }
+
+    printf("Connecting to %s:%d\n",servername,port);
+}
