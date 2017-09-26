95d0e720810b6629c3be6cce741ee137bc64f683
Pedro <pedrocaetan56@gmail.com>
2017-09-26 08:03:25 +0100

    sends LST: currently get connection refused

--- projecto/user.c
+++ projecto/user.c
@@ -6,41 +6,48 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
-#define DEFAULT_PORT 58006
+#define PORT 58006
 #define BUFFER_MAX 128
 
 int main(int argc, char **argv){
-    int port= DEFAULT_PORT,i;
+    int port= PORT,i;
     int fd; //used for comunication
-    char *servername;
+    char servername[128], buffer[128];
 
     struct hostent *hostptr;
     struct sockaddr_in serveraddr; 
 
     for (i = 0; i < argc; i++)
     {
-        printf("%s\n", argv[i]);
         if(strcmp(argv[i],"-p")==0) port=atoi(argv[i+1]);
-        if(strcmp(argv[i],"-n")==0) servername=argv[i+1];
+        if(strcmp(argv[i],"-n")==0) strcpy(servername,argv[i+1]);
     }
 
-    printf("Connecting to %s:%d\n",servername,port);
+
 
     fd=socket(AF_INET,SOCK_STREAM,0); //need to test for return -1
 
     if(servername==NULL){
-        servername= malloc(BUFFER_MAX*sizeof(char));
-        if(gethostname(servername,128)==-1){
+        if(gethostname(servername,BUFFER_MAX)==-1){
             printf("error getting local host name: %s\n",strerror(errno));
             exit(EXIT_SUCCESS);
         }
-
-        printf("Server name: %s",servername);
     }
 
-    hostptr=gethostbyname(servername);
-    
 
-    free(servername);
+    /* structure definition to connect to server*/
+    hostptr=gethostbyname(servername);
+    memset((void*)&serveraddr,(int) '\0',sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = ((struct in_addr *)(hostptr->h_addr_list[0]))->s_addr;
+    serveraddr.sin_port = htons((u_short)PORT);
+
+    if(connect(fd,(struct sockaddr*)&serveraddr,sizeof(serveraddr))==-1) printf("error: %s\n",strerror(errno));
+    write(fd, "LST\n",4);
+    read(fd,buffer,BUFFER_MAX);
+    printf("%s",buffer);
+    close(fd);
+
 }
